c59a09db54eee5d6b4a22d74d42ecd3035dc43b2
rehant <t12.rehan@gmail.com>
2016-12-03 00:28:16 -0500

    Preping to make face3d work

--- Mesh.hpp
+++ Mesh.hpp
@@ -8,7 +8,7 @@
 // My includes
 #include "Colour.hpp"
 #include "Vec3D.hpp"
-//#include "Face3D.hpp"
+#include "Face3D.hpp"
 #include "Vertex2D.hpp"
 #include "Material.hpp"
 
@@ -27,11 +27,12 @@ class Mesh
 
     private:
 	void loadMats(std::string filename); // Loads materials from material libraries
-
-        std::vector<Face3D> *faces; // Mesh faces
+    
+    std::vector<Vec3D> *verts; // Vertex coordinates
 	std::vector<Vertex2D> *texVerts; // Texture vertices
 	std::vector<Vec3D> *norms; // Normals
 	std::vector<Material> *mats; // Materials
+    std::vector<Face3D> *faces; // Mesh faces
 	
 	/* Methods */
 	int convToLongInt(const char* str, long int* out);
