5331548cca03714c925db54e549e64307b16f3ec
rehant <t12.rehan@gmail.com>
2016-12-03 01:04:09 -0500

    Recreated Face3D, althought no destructor in it yet

--- Face3D.cpp
+++ Face3D.cpp
@@ -1,15 +1,89 @@
 #include "Face3D.hpp"
 #include "Vec3D.hpp"
 #include <vector>
+#include <string>
 
 /**
 * Constructs a Face3D with vertex, vertex texture, normal vector indices and material ids
 **/
-Face3D::Face3D()
+Face3D::Face3D(int &v1, int &v2, int &v3, int &t1, int &t2, int &t3, int &n1, int &n2, int &n3, string &mat_id)
+{
+	// Vertex indices
+	this->v1 = v1;
+	this->v2 = v2;
+	this->v3 = v3;
+
+	// Texture indices
+	this->t1 = t1;
+	this->t2 = t2;
+	this->t3 = t3;
+
+	// Normal indices
+	this->n1 = n1;
+	this->n2 = n2;
+	this->n3 = n3;
+
+	// Identifier for current material
+	this->mat_id = mat_id;
+}
 
 
+// Index of v1
+int Face3D::getV1()
+{
+	return v1;
+}
 
-/**
-* Destructor. Destroys a face.
-*/
-//Face3D::~Face3D() {}
+// Index of v2
+int Face3D::getV2()
+{
+	return v2;
+}
+
+// Index of v3
+int Face3D::getV3()
+{
+	return v3;
+}
+
+// Index of t1
+int Face3D::getT1()
+{
+	return t1;
+}
+
+// Index of t2
+int Face3D::getT2()
+{
+	return t2;
+}
+
+// Index of t3
+int Face3D::getT3()
+{
+	return t3;
+}
+
+// Index of n1
+int Face3D::getN1()
+{
+	return n1;
+}
+
+// Index of n2
+int Face3D::getN2()
+{
+	return n2;
+}
+
+// Index of n3
+int Face3D::getN3()
+{
+	return n3;
+}
+
+// Material id of face
+string Face3D::getMAT_ID()
+{
+	return mat_id;
+}
--- Face3D.hpp
+++ Face3D.hpp
@@ -2,36 +2,36 @@
 #define FACE3D_H
 
 #include <vector>
+#include <string>
 #include "Vec3D.hpp"
 
 class Face3D
 {
+	private:
+		int v1, v2, v3, t1, t2, t3, n1, n2, n3;
+		string mat_id;
+
 
 	public:
 		/**
 		* Constructs a Face3D using parsed index data from a triangulated .obj file
 		**/
-		Face3D(int v1, int v2, int v3, int t1, int t2, int t3, int n1, int n2, int n3, std::string mat_id);
-	
-		/**
-		* Destructor. Destroys a face.
-		*/
-		//~Face3D();
+		Face3D(int &v1, int &v2, int &v3, int &t1, int &t2, int &t3, int &n1, int &n2, int &n3, string &mat_id);
 
+		// Vertex indices
 		int getV1();
 		int getV1();
 		int getV1();
 
+		// Texture indices
 		int getT1();
 		int getT2();
 		int getT3();
 
+		// Normal indices
 		int getN1();
 		int getN2();
-		int getN3();
-	
-	private:
-    		
+		int getN3();    		
 };
 
 #endif
